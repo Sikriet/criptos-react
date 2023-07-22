ab2acb88bd10d2e2c6771a5a26bb23acab4e9f78
Sikriet <45955925+Sikriet@users.noreply.github.com>
2023-07-21 21:39:06 -0400

    Primer Commit

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar de Estados Unidos" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GPP", nombre: "Libra Esterlina" },
+  { id: "CLP", nombre: "Peso Chileno" },
+];
+
+export {
+    monedas
+}
